6f870e8990a96c26b32244f5fb1b52b58f0aac63
thomas-ode <thomas.ode.w@gmail.com>
2021-01-13 16:30:01 +0100

    city farmer ok

--- template-p3-back-master/src/routes/index.routes.js
+++ template-p3-back-master/src/routes/index.routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const productsRouter = require('./products.routes.js');
 const buyersRouter = require('./buyers.routes.js');
 const citiesBuyersRouter = require('./citiesBuyers.routes.js');
+const citiesFarmersRouter = require('./citiesFarmers.routes.js');
 
 router.use('/products', productsRouter);
 router.use('/buyers', buyersRouter);
 router.use('/citiesBuyers', citiesBuyersRouter);
+router.use('/citiesFarmers', citiesFarmersRouter)
 
 
 module.exports = router;
