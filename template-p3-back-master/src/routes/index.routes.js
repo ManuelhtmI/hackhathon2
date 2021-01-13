87b9e73b9219b1bba4e0108aae19423cb60c5a6c
thomas-ode <thomas.ode.w@gmail.com>
2021-01-13 16:02:29 +0100

    buyers ok

--- template-p3-back-master/src/routes/index.routes.js
+++ template-p3-back-master/src/routes/index.routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const adminRouter = require('./admin.routes.js');
+const productsRouter = require('./products.routes.js');
+const buyersRouter = require('./buyers.routes.js');
+const citiesBuyersRouter = require('./citiesBuyers.routes.js');
 
-router.use('/admins', adminRouter);
+router.use('/products', productsRouter);
+router.use('/buyers', buyersRouter);
+router.use('/citiesBuyers', citiesBuyersRouter);
+
 
 module.exports = router;
