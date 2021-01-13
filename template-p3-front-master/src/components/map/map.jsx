3f467623abf570af6aba5183381c7c8418bbcca3
thomas-ode <thomas.ode.w@gmail.com>
2021-01-13 16:31:30 +0100

    mapping city buyers/farmers ok

--- template-p3-front-master/src/components/map/map.jsx
+++ template-p3-front-master/src/components/map/map.jsx
@@ -1,7 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './map.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function Map() {
+
+    const [buyers, setBuyers] = useState([])
+    const [farmers, setFarmers] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/citiesBuyers')
+            .then((res) => setBuyers(res.data))
+    }, [])
+
+    useEffect(() => {
+      axios.get('http://localhost:8000/api/citiesFarmers')
+      .then((res) => setFarmers(res.data))
+    }, [])
 
     let setMap = [48.4469, 1.4892]
 
@@ -12,11 +28,20 @@ function Map() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={setMap}>
+        {buyers.map((buyers)=>
+        <Marker position={[buyers.lat, buyers.long]}>
           <Popup>
-            Centre <br /> de la france.
-    </Popup>
-        </Marker>
+            {buyers.lat}
+        </Popup>
+            </Marker>
+        )}
+        {farmers.slice(0,1000).map((farmers)=>
+        <Marker position={[farmers.lat, farmers.long]}>
+          <Popup>
+            {farmers.lat}
+        </Popup>
+            </Marker>
+        )}
       </MapContainer>
     </div>
   );
