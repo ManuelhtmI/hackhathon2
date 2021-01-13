62115f7c95504d9b55e1b7993f8fbfe5b928f04e
Manuel <robinard.manuel@gmail.com>
2021-01-13 09:55:02 +0100

    first commit

---
+++ template-p3-front-master/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AdminList from './components/admin/AdminList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AdminList />
+    </div>
+  );
+}
+
+export default App;
---
+++ template-p3-front-master/src/Fetch.js
@@ -0,0 +1,1 @@
+export const FETCH = 'http://localhost:8000/api';
