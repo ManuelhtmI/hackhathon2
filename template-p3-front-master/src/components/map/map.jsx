2fe93a33f98cea35cef9a9ef6c23b158f375f3d0
Tibo-Wild <thibault.leparc45@gmail.com>
2021-01-15 00:23:07 +0100

    add search box

--- template-p3-front-master/src/components/map/map.jsx
+++ template-p3-front-master/src/components/map/map.jsx
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './map.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import EsriLeafletGeoSearch from "react-esri-leaflet/plugins/EsriLeafletGeoSearch";
 
 
 function Map() {
@@ -22,27 +23,36 @@ function Map() {
     let setMap = [48.4469, 1.4892]
 
   return (
-    <div id="mapid">
-      <MapContainer center={setMap} zoom={7} className="map">
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {buyers.map((buyers)=>
-        <Marker position={[buyers.lat, buyers.long]}>
-          <Popup>
-            {buyers.lat}
-        </Popup>
-            </Marker>
-        )}
-        {farmers.slice(0,1000).map((farmers)=>
-        <Marker position={[farmers.lat, farmers.long]}>
-          <Popup>
-            {farmers.lat}
-        </Popup>
-            </Marker>
-        )}
-      </MapContainer>
+    <div>
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.4/dist/esri-leaflet-geocoder.css"></link>
+      </head>
+      <div id="mapid">
+        <MapContainer center={setMap} zoom={7} className="map">
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {buyers.map((buyers)=>
+          <Marker position={[buyers.lat, buyers.long]}>
+            <Popup>
+              {buyers.lat}
+          </Popup>
+              </Marker>
+          )}
+          {farmers.slice(0,1000).map((farmers)=>
+          <Marker position={[farmers.lat, farmers.long]}>
+            <Popup>
+              {farmers.lat}
+          </Popup>
+              </Marker>
+          )}
+          <EsriLeafletGeoSearch
+            position="topleft"
+            useMapBounds={false}
+          />
+        </MapContainer>
+      </div>
     </div>
   );
 }
