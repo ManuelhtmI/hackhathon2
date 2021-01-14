364e98c4d2d6fefc6761a162b799a081eb3c1aaf
fatima-dah <dahnini.fatima@hotmail.fr>
2021-01-14 09:35:32 +0100

    ok

--- template-p3-front-master/src/App.jsx
+++ template-p3-front-master/src/App.jsx
@@ -1,12 +1,12 @@
-// import Map from "./components/map/map.jsx";
+import Map from "./components/map/map.jsx";
 import Header from "./components/Header/Header.jsx";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      {/* <Map /> */}
+      <Map />
     </div>
   );
 }
