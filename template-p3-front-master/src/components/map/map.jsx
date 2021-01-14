6c34ef3f2506589990761ea238e305bdf89fbaa0
thomas-ode <thomas.ode.w@gmail.com>
2021-01-14 18:38:29 +0100

    popup ok

--- template-p3-front-master/src/components/map/map.jsx
+++ template-p3-front-master/src/components/map/map.jsx
@@ -4,73 +4,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import L from 'leaflet';
-
 import markerBuyer from '../assets/euros.png';
-
-import man1 from '../assets/Man1_1.png';
-import man2 from '../assets/Man1_2.png';
-import man3 from '../assets/Man1_3.png';
-import man4 from '../assets/Man1_4.png';
-import man5 from '../assets/Man1_5.png';
-import man6 from '../assets/Man1_6.png';
-import man7 from '../assets/Man1_7.png';
-import man8 from '../assets/Man1_8.png';
-import man9 from '../assets/Man2_1.png';
-import man10 from '../assets/Man2_2.png';
-import man11 from '../assets/Man2_3.png';
-import man12 from '../assets/Man2_4.png';
-import man13 from '../assets/Man2_5.png';
-import man14 from '../assets/Man2_6.png';
-import man15 from '../assets/Man2_7.png';
-import man16 from '../assets/Man2_8.png';
-import man17 from '../assets/Man3_1.png';
-import man18 from '../assets/Man3_2.png';
-import man19 from '../assets/Man3_3.png';
-import man20 from '../assets/Man3_4.png';
-import man21 from '../assets/Man3_5.png';
-import man22 from '../assets/Man3_6.png';
-import man23 from '../assets/Man3_7.png';
-import man24 from '../assets/Man3_8.png';
-import woman1 from '../assets/Woman1_1.png';
-import woman2 from '../assets/Woman1_2.png';
-import woman3 from '../assets/Woman1_3.png';
-import woman4 from '../assets/Woman1_4.png';
-import woman5 from '../assets/Woman1_5.png';
-import woman6 from '../assets/Woman1_6.png';
-import woman7 from '../assets/Woman1_7.png';
-import woman8 from '../assets/Woman1_8.png';
-import woman9 from '../assets/Woman2_1.png';
-import woman10 from '../assets/Woman2_2.png';
-import woman11 from '../assets/Woman2_3.png';
-import woman12 from '../assets/Woman2_4.png';
-import woman13 from '../assets/Woman2_5.png';
-import woman14 from '../assets/Woman2_6.png';
-import woman15 from '../assets/Woman2_7.png';
-import woman16 from '../assets/Woman2_8.png';
-import woman17 from '../assets/Woman3_1.png';
-import woman18 from '../assets/Woman3_2.png';
-import woman19 from '../assets/Woman3_3.png';
-import woman20 from '../assets/Woman3_4.png';
-import woman21 from '../assets/Woman3_5.png';
-import woman22 from '../assets/Woman3_6.png';
-import woman23 from '../assets/Woman3_7.png';
-import woman24 from '../assets/Woman3_8.png';
-
 import home from '../assets/home.png';
-
 import iconFarmers from '../assets/66307.png';
-
 
 function Map() {
 
   const [buyers, setBuyers] = useState([])
   const [farmers, setFarmers] = useState([])
-
-  // const avatar = [man1, man2, man3, man4, man5, man6, man7, man8, man9, man10, man11, man12, man13, man14, man15, man16, man17, man18, man19, man20, man21, man22, man23, man24, woman1, woman2, woman3, woman4, woman5, woman6, woman7, woman8, woman9, woman10, woman11, woman12, woman13, woman14, woman15, woman16, woman17, woman18, woman19, woman20, woman21, woman22, woman23, woman24]
-
-  // const random = avatar[Math.floor(Math.random() * avatar.length)];
-
-
 
   const markerBuyers = L.icon({
     iconUrl: markerBuyer,
@@ -133,9 +74,13 @@ function Map() {
           {farmers.map((farmers) =>
             <Marker position={[farmers.lat, farmers.long]} icon={farmerBuyers}>
               <Popup>
-                <h2>Taille de l'exploitation: {farmers.farm_size}ha</h2>
-                <h2>Dernier article vendu : {farmers.name} <br />le: {farmers.created_at}</h2>
-                <h2>Inscrit depuis le: {farmers.registered_at}</h2>
+                <h2 className="farmerText">Taille de l'exploitation: {farmers.farm_size}ha</h2>
+                <h2 className="farmerText">Dernier article vendu : {farmers.name}</h2>
+                <h2 className="farmerText">le: {farmers.created_at}</h2>
+                <h2 className="farmerText">Inscrit depuis le: {farmers.registered_at}</h2>
+                <div className="farmerCategory">
+                <img src={farmers.avatar} alt={farmers.name} className="farmerAvatar"/>
+                </div>
               </Popup>
             </Marker>
 
