2505ccbc308082f90367e54360c4ebc1b597a7e8
thomas-ode <thomas.ode.w@gmail.com>
2021-01-15 03:04:14 +0100

    radius and localisation

--- template-p3-front-master/src/components/Header/Header.jsx
+++ template-p3-front-master/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.svg";
 import Search from "../../assets/search_logo.png";
--- template-p3-front-master/src/components/map/map.jsx
+++ template-p3-front-master/src/components/map/map.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
 import './map.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -7,11 +7,40 @@ import L from 'leaflet';
 import markerBuyer from '../assets/euros.png';
 import home from '../assets/home.png';
 import iconFarmers from '../assets/66307.png';
+import useGeoLocation from '../hooks/useGeolocation';
 
 function Map() {
 
   const [buyers, setBuyers] = useState([])
   const [farmers, setFarmers] = useState([])
+  const location = useGeoLocation();
+  const [countClose, setCountClose] = useState(0);
+  const [latMin, setLatMin] = useState(0)
+  const [latMax, setLatMax] = useState(0)
+  const [lngMin, setLngMin] = useState(0)
+  const [lngMax, setLngMax] = useState(0)
+
+  useEffect(() => {
+    if (location.loaded === true) {
+      setLatMin(location.coordinates.lat - 0.180227)
+      setLatMax(location.coordinates.lat + 0.180227)
+      setLngMin(location.coordinates.lng - 0.246349)
+      setLngMax(location.coordinates.lng + 0.246349)
+    } else {
+      setLatMin(0)
+      setLatMax(0)
+      setLngMin(0)
+      setLngMax(0)
+    }
+  }, [location])
+
+  let countTrans = farmers.filter(function (farmer) {
+    return (farmer.lat <= latMax && farmer.lat >= latMin && farmer.long <= lngMax && farmer.long >= lngMin)
+  })
+
+  useEffect(() => {
+    setCountClose(countTrans.length)
+  }, [countTrans])
 
   const markerBuyers = L.icon({
     iconUrl: markerBuyer,
@@ -31,7 +60,6 @@ function Map() {
     iconAnchor: [40, 60],
   });
 
-
   useEffect(() => {
     axios.get('http://localhost:8000/api/citiesBuyers')
       .then((res) => setBuyers(res.data))
@@ -40,14 +68,13 @@ function Map() {
   useEffect(() => {
     axios.get('http://localhost:8000/api/citiesFarmers')
       .then((res) => setFarmers(res.data))
-  }, [])
-
+  }, []);
 
   let setMap = [48.4469, 1.4892]
 
   return (
     <div id="mapid">
-      <MapContainer center={setMap} zoom={10} className="map">
+      <MapContainer center={setMap} zoom={9} className="map">
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -57,8 +84,15 @@ function Map() {
             <a href="https://comparateuragricole.com/"><h2>ComparateurAgricole.com</h2></a>
           </Popup>
         </Marker>
+        {location.loaded === true ?
+          <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
+            <Popup>
+              <h2>{countClose} transactions ont eu lieu près de chez vous !</h2>
+            </Popup>
+            <Circle center={[location.coordinates.lat, location.coordinates.lng]} radius={20000} />
+          </Marker>
+          : null}
         {buyers.map((buyers) =>
-
           <Marker position={[buyers.lat, buyers.long]} icon={markerBuyers}>
             <Popup>
               <h1>{buyers.name}</h1>
@@ -71,7 +105,7 @@ function Map() {
             console.warn('cluster-click', cluster, cluster.layer.getAllChildMarkers())
           }
         >
-          {farmers.map((farmers) =>
+          {farmers.slice(0, 1000).map((farmers) =>
             <Marker position={[farmers.lat, farmers.long]} icon={farmerBuyers}>
               <Popup>
                 <h2 className="farmerText">Taille de l'exploitation: {farmers.farm_size}ha</h2>
@@ -79,11 +113,10 @@ function Map() {
                 <h2 className="farmerText">le: {farmers.created_at}</h2>
                 <h2 className="farmerText">Inscrit depuis le: {farmers.registered_at}</h2>
                 <div className="farmerCategory">
-                <img src={farmers.avatar} alt={farmers.name} className="farmerAvatar"/>
+                  <img src={farmers.avatar} alt={farmers.name} className="farmerAvatar" />
                 </div>
               </Popup>
             </Marker>
-
           )}
         </MarkerClusterGroup>
       </MapContainer>
